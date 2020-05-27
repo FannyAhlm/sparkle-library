05c2af6d47f21f194f2907fb5e8c12ca4f9f207b
Karin Karlsson <karin.drake@gmail.com>
2020-05-27 15:09:45 +0200

    Updates

---
+++ precache-manifest.c4a12199c24b45acb41de5277f1487da.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6023e706f36ad88abe333b8679b7a584",
+    "url": "/sparkle-library/index.html"
+  },
+  {
+    "revision": "7e93da17b9d519d35649",
+    "url": "/sparkle-library/static/css/main.3031d609.chunk.css"
+  },
+  {
+    "revision": "a1b3597b9ef2718f467f",
+    "url": "/sparkle-library/static/js/2.b0640a66.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/sparkle-library/static/js/2.b0640a66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e93da17b9d519d35649",
+    "url": "/sparkle-library/static/js/main.ce83a5af.chunk.js"
+  },
+  {
+    "revision": "ec51b5596017448027e4",
+    "url": "/sparkle-library/static/js/runtime-main.f582bf94.js"
+  },
+  {
+    "revision": "569ae7b3d2096dadb337b3f5c506c819",
+    "url": "/sparkle-library/static/media/LogoBig.569ae7b3.png"
+  },
+  {
+    "revision": "cabd5147e0951c9ec64546a137d245e6",
+    "url": "/sparkle-library/static/media/music.cabd5147.png"
+  }
+]);
